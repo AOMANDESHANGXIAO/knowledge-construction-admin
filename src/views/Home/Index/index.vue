cee9a487676d6469393303c441873dd786befcf5
tom <13849343+tomFfff@user.noreply.gitee.com>
2024-07-03 10:58:22 +0800

    feat:(班级管理查询班级列表功能实现)

---
+++ src/views/Home/Index/index.vue
@@ -0,0 +1,97 @@
+<script lang="ts" setup>
+import {ChatLineRound, Coffee, DataLine, Document, Location, Notification, Setting} from "@element-plus/icons-vue";
+import {useCssVar} from "@vueuse/core";
+
+defineOptions({
+  name: 'homeIndex',
+})
+const coolBlack = useCssVar('--st-cool-black-color')
+</script>
+
+<template>
+  <el-row class="st-home-container">
+    <div class="menu-tab">
+      <div class="title">思维之流</div>
+      <el-menu class="st-home-menu" active-text-color="#fff" :background-color="coolBlack" text-color="#fff" router>
+        <el-menu-item index="3">
+          <section class="st-menu-item">
+              <el-icon><Notification /></el-icon>
+            <span>班级管理</span>
+          </section>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <section class="st-menu-item">
+            <el-icon><ChatLineRound /></el-icon>
+          <span>讨论管理</span>
+          </section>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <section class="st-menu-item">
+            <el-icon><DataLine /></el-icon>
+          <span>学习分析</span>
+          </section>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <section class="st-menu-item">
+            <el-icon><Coffee /></el-icon>
+            <span>管理员</span>
+          </section>
+        </el-menu-item>
+      </el-menu>
+    </div>
+    <div class="router-container">
+      <router-view />
+    </div>
+  </el-row>
+</template>
+
+<style lang="scss" scoped>
+.st-home-container {
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: pink;
+
+  .menu-tab {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 150px;
+    .title {
+      width: 100%;
+      height: 50px;
+      color: #ffffff;
+      font-size: 20px;
+      font-weight: 700;
+      text-align: center;
+      line-height: 50px;
+      background-color: var(--st-cool-black-color);
+    }
+  }
+  .router-container {
+    flex: 1;
+    height: 100%;
+  }
+
+  .st-home-menu {
+    height: 100%;
+    .st-menu-item {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      height: 100%;
+    }
+
+    &:deep(.el-menu-item) {
+      padding: 0;
+      &:hover {
+        background-color: var(--st-theme-color);
+      }
+    }
+    &:deep(.el-menu-item.is-active) {
+      background-color: var(--st-theme-color);
+    }
+  }
+}
+</style>
